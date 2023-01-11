5d9300ccdcd5a58e29bfb845b90c2f38d87e03c3
Sourabh Upadhye <sourabh.1998upadhye@gmail.com>
2023-01-11 13:55:27 +0530

    UPD : Website is up

---
+++ src/pages/home/etlIndex.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const EtlBlock = () => {
+  return (
+    <div class="containerx">
+      <div class="holder">
+        <div class="box"></div>
+      </div>
+      <div class="holder">
+        <div class="box"></div>
+      </div>
+      <div class="holder">
+        <div class="box"></div>
+      </div>
+    </div>
+  );
+};
